5dabc4b0b626ee8a3e4a75e873cbf6418d3b1789
zekroTJA <riho@live.de>
2023-05-07 20:55:28 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         'Topic :: Software Development :: Libraries',
     ],
